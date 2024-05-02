a0c537b4d7c8ab4ea6c027506440bee7f4406ec2
RitvikSriv <ritvik123srivastava@gmail.com>
2024-05-02 21:07:09 +0530

    completed responsive filters

---
+++ src/constants/constants.js
@@ -0,0 +1,107 @@
+export const ROLES_OPTIONS_MAP = [
+  { label: "Backend", id: "1" },
+  { label: "Frontend", id: "2" },
+  { label: "Fullstack", id: "3" },
+  { label: "Android", id: "5" },
+  { label: "IOS", id: "6" },
+  { label: "Flutter", id: "7" },
+  { label: "React Native", id: "8" },
+  { label: "Tech Lead", id: "9" },
+  { label: "Dev-Ops", id: "10" },
+  { label: "Data Engineer", id: "11" },
+  { label: "Data Science", id: "12" },
+];
+
+export const HEADCOUNT_OPTIONS_MAP = [
+  { label: "1-10", id: "1" },
+  { label: "11-20", id: "2" },
+  { label: "21-50", id: "3" },
+  { label: "51-100", id: "4" },
+  { label: "101-200", id: "5" },
+  { label: "201-500", id: "6" },
+  { label: "500+", id: "7" },
+];
+
+export const EXPERIENCE_OPTIONS_MAP = [
+  {
+    label: "1",
+    id: "1",
+  },
+  {
+    label: "2",
+    id: "2",
+  },
+  {
+    label: "3",
+    id: "3",
+  },
+  {
+    label: "4",
+    id: "4",
+  },
+  {
+    label: "5",
+    id: "5",
+  },
+  {
+    label: "6",
+    id: "6",
+  },
+  {
+    label: "7",
+    id: "7",
+  },
+  {
+    label: "8",
+    id: "8",
+  },
+  {
+    label: "9",
+    id: "9",
+  },
+  {
+    label: "10",
+    id: "10",
+  },
+];
+
+export const MODE_OPTIONS_MAP = [
+  { label: "Remote", id: "1" },
+  { label: "Hybrid", id: "2" },
+  { label: "In-office", id: "3" },
+];
+
+export const SALARY_OPTIONS_MAP = [
+  {
+    label: "0L",
+    id: "1",
+  },
+  {
+    label: "10L",
+    id: "2",
+  },
+  {
+    label: "20L",
+    id: "3",
+  },
+  {
+    label: "30L",
+    id: "4",
+  },
+  {
+    label: "40L",
+    id: "5",
+  },
+  {
+    label: "50L",
+    id: "6",
+  },
+  {
+    label: "60L",
+    id: "7",
+  },
+  {
+    label: "70L",
+    id: "8",
+  },
+];
